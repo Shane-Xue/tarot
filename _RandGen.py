6e97343f345c5fcd29475e77c4aabe155ba82c61
unknown <qq2198631773@outlook.com>
2021-01-31 19:01:35 +0800

    Update _RandGen.py 1. Corrected a mistake in the SmallToBig lambda: used a tuple 2. Corrected (ammended) the random generation method 3. Added a testing function 4. Tested (as a dice). std was mean 111 with 1~6; the std decreased with the increase of the range (strangely enough) 5. Added a testing function for the np.random.randint() method

--- _RandGen.py
+++ _RandGen.py
@@ -2,9 +2,10 @@
 random number generator wrapper class
 """
 import math
+import numpy as np
 from random import random
 
-SmallToBig = lambda a,b : a,b if a < b else b,a
+SmallToBig = lambda a,b : (a,b) if a < b else (b,a)
 
 class RandGen():
     def __init__(self , ceil, floor = 0 , randness = 3):
@@ -18,4 +19,24 @@ class RandGen():
             self.randness = math.log(self.range,10) + 3
     
     def gen(self):
-        return (random() * (10 ** self.randness) % self.range) + self.floor
+        return (math.ceil(random() * (10 ** self.randness) * self.range + 1) % self.range) + self.floor
+
+def _TestGen(floor,ceil,randness=3,tests=10**5):
+    SmallToBig(floor,ceil)
+    rg=RandGen(ceil,floor,randness)
+    lst=list()
+    for a in range(floor,ceil+1):
+        lst.append(0)
+    for a in range(tests):
+        lst[rg.gen()-floor]+=1
+    return lst
+
+def _TestGenNp(floor,ceil,tests=10**5):
+    lst=[]
+    for a in range(floor,ceil+1):
+        lst.append(0)
+    for a in range(tests):
+        lst[np.random.randint(floor,ceil)-floor]+=1
+    return lst
+
+def _Suites()
